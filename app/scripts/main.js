49d91747f9e21f7a4256bc0f03cfad295442a8ee
OJay Robinson <ojay@gskinner.com>
2015-08-17 12:02:29 -0600

    Yeoman generated scaffolding existing content moved to /firstversion

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global OER, $*/
+
+
+window.OER = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    OER.init();
+});
