9c7f2cebb7029d8cab57f6d4e2f6a5d78f737459
Lingbo Tang <lingbo19.tang@gmail.com>
2015-08-20 15:23:53 -0600

    RLOModel and RLOCollection first version is passed.

--- app/scripts/collections/RLOCollection.js
+++ app/scripts/collections/RLOCollection.js
@@ -7,8 +7,20 @@ OER.Collections = OER.Collections || {};
 
     OER.Collections.RLOCollection = Backbone.Collection.extend({
 
-        model: OER.Models.RLOCollection
+        model: OER.Models.RLOModel,
 
-    });
+
+		// We keep the Oers in grid order.
+		// GUID in the database. This generates the next order number for new item.
+		nextOrder:function() {
+			return this.length ? this.last().get('order') + 1 : 1;
+		},
+
+		// Oers are sorted by their original insertion order.
+		comparator: 'order'
+	});
+
+	// Create our global collection of **oers**
+	// OER.Collections = new OER.Collections.RLOCollection();
 
 })();
--- app/scripts/models/RLOModel.js
+++ app/scripts/models/RLOModel.js
@@ -5,17 +5,31 @@ OER.Models = OER.Models || {};
 (function () {
     'use strict';
 
+    // Oer Model
+    // ---------
+
+    // Our basic **Todo** model has `title`, `order`, `selected`, `hovered`
+    // and `info` attributes. 
     OER.Models.RLOModel = Backbone.Model.extend({
 
         url: '',
 
-        initialize: function() {
+        // Default attributes for the oer
+        // and ensure that each oer created `title` and `selected` keys.
+        // Also, it might contain more extended info.
+        defaults: {
+            title: '',
+            preview: '',
+            info: ''
         },
 
-        defaults: {
+        // Make it a rectangular div and `hovered` state of this oer item
+        initialize: function() {
+            //alert("Initialized");
         },
 
         validate: function(attrs, options) {
+
         },
 
         parse: function(response, options)  {
