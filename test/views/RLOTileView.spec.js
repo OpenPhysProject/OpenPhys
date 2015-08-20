56b0ddce646d94e7b9b0b60da4aa722aad1c0483
OJay Robinson <ojayrob@gmail.com>
2015-08-20 15:27:36 -0600

    RLOTileView generated with yo

---
+++ test/views/RLOTileView.spec.js
@@ -0,0 +1,10 @@
+/*global beforeEach, describe, it, assert, expect  */
+'use strict';
+
+describe('RLOTileView View', function () {
+
+    beforeEach(function () {
+        this.RLOTileViewView = new OER.Views.RLOTileView();
+    });
+
+});
