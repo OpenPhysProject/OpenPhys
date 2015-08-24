8a54924f9b680c9324280578837a071245c4f042
OJay Robinson <ojayrob@gmail.com>
2015-08-24 14:15:46 -0600

    update Main structure to different approach, with added functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,64 @@
+(function (scope) {
+    "use strict";
+    
+    var Application = function () {
+        this.init();
+    };
+    var p = Application.prototype;
+    
+    p.Models = {};
+    p.Collections = {};
+    p.Views = {};
+    p.Routers = {};
+    
+    p.homeView = null;
+    p.RLOView = null;
+    p.loader = null;
+    
+    p.RLOs = null;
+    
+    p.init = function () {
+        this.loader = new scope.Loader();
+        this.loader.hide();
+        this.homeView = $(".RLOList");
+        
+        this.loadData();
+        this.createTileView();
+        
+        // todo start preloading assets with PreloadJS
+        // todo setup router and route handling
+        this.showHomeView(); // OJR when route handling is in, this may need to route to different RLO views instead
+    };
+    
+    p.loadData = function () {
+        // todo load real data
+        
+        //placeholder data
+        this.RLOs = new scope.Collections.RLOCollection();
+        for (var i = 0; i < 12; i++) {
+            var m = new scope.Models.RLOModel({"title":"title " + i});
+            this.RLOs.add(m);
+        }
+    };
+    
+    p.createTileView = function() {
+        // todo use data to create views
+    };
+    
+    p.showHomeView = function() {
+        
+    };
+    
+    p.showRLOView = function() {
+        
+    };
+    
+    scope.Application = Application;
 
-window.OER = {
-    Models: {},
-    Collections: {},
-    Views: {},
-    Routers: {},
-    init: function () {
-        'use strict';
-        console.log('Hello from Backbone!');
-    }
-};
+})(window.OER = window.OER || {});
 
 $(document).ready(function () {
     'use strict';
-    OER.init();
+    
+    var app = new OER.Application();
 });
