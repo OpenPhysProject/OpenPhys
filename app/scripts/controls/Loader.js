429e4a19b9419723cb5eb29c112c0c494761e765
Annieday <adai1@ualberta.ca>
2015-08-24 11:25:39 -0600

    Clean up test code and comment. Change the way to load style file.

--- app/scripts/controls/Loader.js
+++ app/scripts/controls/Loader.js
@@ -1,36 +1,31 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* Loader control */
 
 var OER = OER || {};
 (function () {
     'use strict';
-    
-    var Loader =  function(){
-        this.init = function(){
-            if ( this._loader === null ) {
-                this._loader = $('.loader');
-            }
-        };
+
+    var Loader = function () {
         this.init();
     };
     var p = Loader.prototype;
-    
+
     p._loader = null;
     
-    p.show = function(){
-       // this._loader.show();
+    p.init = function () {
+        if (this._loader === null) {
+            this._loader = $('.loader');
+        }
+    };
+    
+    p.show = function () {
         this._loader.removeClass('out');
         this._loader.addClass('in');
     };
-    
-    p.hide = function(){
-       // this._loader.hide();
+
+    p.hide = function () {
         this._loader.removeClass('in');
         this._loader.addClass('out');
     };
-    
-    OER.Loader =  Loader;
+
+    OER.Loader = Loader;
 })();
