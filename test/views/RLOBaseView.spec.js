cb9536c005498f8ec216dd0ae0af697669b544bb
OJay Robinson <ojayrob@gmail.com>
2015-08-26 11:58:36 -0600

    yo generated RLO base view

---
+++ test/views/RLOBaseView.spec.js
@@ -0,0 +1,10 @@
+/*global beforeEach, describe, it, assert, expect  */
+'use strict';
+
+describe('RLOBaseView View', function () {
+
+    beforeEach(function () {
+        this.RLOBaseViewView = new OER.Views.RLOBaseView();
+    });
+
+});
