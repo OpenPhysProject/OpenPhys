32ff602a151124bb0a5539730e45c6fd2adf93b7
OJay Robinson <ojayrob@gmail.com>
2015-08-27 13:22:59 -0600

    Update RLOBaseView template to match design discussion yo generated NavView, NavCardView, NavCardModel, and NavCardCollection

---
+++ test/views/NavCardView.spec.js
@@ -0,0 +1,10 @@
+/*global beforeEach, describe, it, assert, expect  */
+'use strict';
+
+describe('NavCardView View', function () {
+
+    beforeEach(function () {
+        this.NavCardViewView = new OER.Views.NavCardView();
+    });
+
+});
---
+++ test/views/NavView.spec.js
@@ -0,0 +1,10 @@
+/*global beforeEach, describe, it, assert, expect  */
+'use strict';
+
+describe('NavView View', function () {
+
+    beforeEach(function () {
+        this.NavViewView = new OER.Views.NavView();
+    });
+
+});
