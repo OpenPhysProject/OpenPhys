4e26331feccf11005ed2096cbcb7e6e298eb0959
OJay Robinson <ojayrob@gmail.com>
2015-08-28 16:48:45 -0600

    Updated RLOBaseView with lastest structure

--- app/scripts/views/RLOBaseView.js
+++ app/scripts/views/RLOBaseView.js
@@ -5,21 +5,45 @@ OER.Views = OER.Views || {};
 (function () {
     'use strict';
 
-    OER.Views.RLOBaseView = Backbone.View.extend({
-
-        template: JST['app/scripts/templates/RLOBaseView.ejs'],
-
-        events: {},
-
-        initialize: function () {
-            //this.listenTo(this.model, 'change', this.render); // OJR currently model will not change
-            this.render();
-        },
-
-        render: function () {
-            this.setTemplate(this.template(this.model.toJSON()));
-        }
-
-    });
-
+    var p = {};
+    var s = {};
+    
+    p.template = JST['app/scripts/templates/RLOBaseView.ejs'];
+    
+    p.navView = null;
+    p.content = null;
+    p.contentContainer = null;
+    p.currentView = null;
+    
+    p.events = {
+        
+    };
+    
+    p.initialize = function() {
+        this.contentContainer = $("#rlo-base-content-container", this.$el);
+        //this.currentView = "intro";   // OJR this will depend on how we set up the model
+        this.setSubViews();
+    };
+    
+    p.setSubViews = function() {
+        this.navView = new OER.Views.NavView(this.model);
+        
+        //this.content = new OER.Views[this.currentView]();
+    };
+    
+    p.updateModel = function(newModel) {
+      this.model = newModel;
+      this.content.remove();
+      this.navView.remove();
+      
+      //set this.currentView
+      
+      this.setSubViews();
+    };
+    
+    p.render = function() {
+        this.setTemplate(this.template(this.model.toJSON()));
+    };
+    
+    OER.Views.RLOBaseView = Backbone.View.extend(p, s);
 })();
