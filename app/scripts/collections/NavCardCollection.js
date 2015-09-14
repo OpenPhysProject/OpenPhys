4c02dacc9e09a9e5fd2b6371410045a734c76fe9
OJay Robinson <ojayrob@gmail.com>
2015-09-14 11:49:46 -0600

    RLOModel now maintains a single current NavCardView across contentMap

--- app/scripts/collections/NavCardCollection.js
+++ app/scripts/collections/NavCardCollection.js
@@ -9,18 +9,31 @@ OER.Collections = OER.Collections || {};
         
         model: OER.Models.NavCardModel,
         
+        lastCurrent: null,
+        
         initialize: function() {
             this.lastCurrent = null;
             this.on( "change:current", this.setLastCurrent, this);
         },
         
         setLastCurrent: function(model) {
-            if(model.get('current')){
+            if(model.get("current")){
                 if (this.lastCurrent) {
                     this.lastCurrent.toggleCurrent();
                 }
-                    this.lastCurrent = model;
+                this.lastCurrent = model;
             }
+        },
+        
+        /**
+         * Turn off the current property in NavCardModel and unset
+         * @returns {undefined}
+         */
+        removeCurrent: function() {
+            if (this.lastCurrent) {
+                this.lastCurrent.toggleCurrent();
+            }
+            this.lastCurrent = null;
         },
         
         // Filter down the list of all the visited card are finished.
@@ -38,7 +51,7 @@ OER.Collections = OER.Collections || {};
         
         // Keep navigation card in grid order.
         nextOrder: function () {
-            return this.length ? this.last().get('order') + 1 : 1;
+            return this.length ? this.last().get("order") + 1 : 1;
         },
         
         // Card models are sorted by their original insertion order.
--- app/scripts/models/RLOModel.js
+++ app/scripts/models/RLOModel.js
@@ -11,7 +11,12 @@ OER.Models = OER.Models || {};
             selected: false,
             info: '',
             primaryPathIndex: null,
-            contentMap: null  // 2D array of NavCardCollection
+            contentMap: null,  // 2D array of NavCardCollection
+            lastCurrentCollection: null,
+        },
+        
+        initialize: function(){
+            this.on("change:contentMap", this.setContentMapListeners);
         },
 
         getPrimaryContent: function() {
@@ -20,7 +25,33 @@ OER.Models = OER.Models || {};
         
         getContentByIndex: function(index){
             return this.get('contentMap')[index];
-        }
+        },
+        
+        /**
+         * add change listeners to all NavCardCollections to handle keeping a single current
+         * @returns {undefined}
+         */
+        setContentMapListeners: function() {
+            // TODO if contentMap could ever change, add .off from previous collection
+            var cm = this.get("contentMap");
+            for(var l = cm.length; l--; ) {
+                cm[l].on("change:current", this.handleCurrentChange, this);
+            }
+        },
+        
+        /**
+         * update NavCardCollections to keep only one current NavCard accross all
+         * @returns {undefined}
+         */
+        handleCurrentChange: function(model) {
+            var navCardCollection = model.collection;
+            if(navCardCollection.lastCurrent != null){
+                if (this.lastCurrentCollection && this.lastCurrentCollection != navCardCollection) {
+                    this.lastCurrentCollection.removeCurrent();
+                }
+                this.lastCurrentCollection = navCardCollection;
+            }
+        },
         
     });
 
