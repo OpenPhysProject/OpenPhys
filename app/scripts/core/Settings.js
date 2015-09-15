120ea57204f32f3b841c49c489057260e7702fe4
Annieday <adai1@ualberta.ca>
2015-09-15 14:31:23 -0600

    edit animation duration settings

--- app/scripts/core/Settings.js
+++ app/scripts/core/Settings.js
@@ -1,12 +1,29 @@
+
+
+
+
 (function (scope) {
     "use strict";
+    var t = {
+        duration_loop: 1200,
+        duration_longer: 800,
+        duration_main_to_content: 700,
+        duration_long: 500,
+        duration_default: 300,
+        duration_close_nav: 250,
+        duration_short: 100
+    };
+
 
     var s = {};
 
+
+
+
     // Timing in milliseconds
-    s.MAIN_TO_CONTENT = 700;
-    s.CLOSE_NAV = 250;
-        
+    s.MAIN_TO_CONTENT = t.duration_main_to_content;
+    s.CLOSE_NAV = t.duration_close_nav;
+
     scope.settings = s;
 
 }(window.OER = window.OER || {}));
