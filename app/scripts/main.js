34336ec83b0c29d8fbb20b3b658b29ac4737d04f
Annieday <adai1@ualberta.ca>
2015-10-08 16:11:52 -0600

    fix a bug.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -135,8 +135,8 @@ $(document).ready(function () {
 
     MathJax.Hub.Config({
         config: ["MMLorHTML.js"],
-        jax: ["input/TeX","input/MathML","output/HTML-CSS","output/NativeMML", "output/CommonHTML"],
-        extensions: ["tex2jax.js","mml2jax.js","MathMenu.js","MathZoom.js", "CHTML-preview.js"],
+        jax: ["input/TeX","input/MathML","output/HTML-CSS","output/NativeMML"],
+        extensions: ["tex2jax.js","mml2jax.js","MathMenu.js","MathZoom.js"],
         TeX: {
           extensions: ["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js"]
         }
