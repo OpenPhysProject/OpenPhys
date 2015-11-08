257fe21425dc009b80b65168f3475ce31d89757e
Jonathan <jcsharp@ualberta.ca>
2015-11-08 15:59:37 -0700

    RLO1: Clean up text and presentation of physics on many pages

--- app/scripts/data/RLO1.js
+++ app/scripts/data/RLO1.js
@@ -2,7 +2,7 @@
     var s = {};
     
     s.title = "The Electronic Structure of the Atom";
-    s.preview = "/images/RLO1/Charge_and_Field_001.png"; //svg image
+    s.preview = "/images/RLO1/AtomPlay.svg"; //svg image
     s.route = "ElectronicStructureOfTheAtom";
     s.info = "";
     s.primaryPathIndex = 3;
@@ -35,20 +35,20 @@
             {title:"", image:"", route:""},
             {title:"", image:"", route:""},
             {title:"", image:"", route:""},          
-            {title:"300.3 Classical EM", image:"", route:"300_3"},
+            {title:"300.3 Classical Electromagnetism", image:"", route:"300_3"},
             {title:"300.4 Angular Momentum", image:"", route:"300_4"},
             {title:"300.5 Math of Classical Standing Waves", image:"", route:"StandingWavesMath"},
             {title:"", image:"", route:""}, 
             {title:"300.7 Quantum Mechanics", image:"", route:"300_7"},           
         ],
         
-    // index 3 
+    // Row 3 index 3 
         [
-           {title:"400.0 Intro",        image:"", route:"400_0"},
-           {title:"400.1 Dalton's Atoms",       image:"", route:"400_1"},
-           {title:"400.2 Plum Pudding Atom", image:"", route:"400_2"},  
-           {title:"400.3 Solar System Atom", image:"", route:"400_3"}, 
-           {title:"400.4 Bohr Atom", image:"", route:"400_4"}, 
+           {title:"400.0 Atomic Models",        image:"", route:"400_0"},
+           {title:"400.1 Dalton's Atoms (1805)",  image:"", route:"400_1"},
+           {title:"400.2 Plum Pudding Atom (1904)", image:"", route:"400_2"},  
+           {title:"400.3 Solar System Atom (1910's)", image:"", route:"400_3"}, 
+           {title:"400.4 Bohr Quantized Atom (1913)", image:"", route:"400_4"}, 
            {title:"400.5 QM Atom 1: Matter Waves", image:"", route:"400_5"},
            {title:"400.6 QM Atom 2: Nuclear Potential", image:"", route:"NuclearPotential"}, 
            {title:"400.7 QM Atom 3: Trapped Waves", image:"", route:"400_7"},
@@ -60,8 +60,8 @@
            {title:"",        image:"", route:"500_0"},
            {title:"",       image:"", route:"500_1"},
            {title:"",       image:"", route:"500_2"},
-           {title:"500.3 Failure of Classical Orbital", image:"", route:"500_3"},
-           {title:"500.4 Bohr Model with Equations", image:"", route:"500_4"}, 
+           {title:"500.3 Cracks", image:"", route:"500_3"},
+           {title:"", image:"", route:"500_4"}, 
            {title:"",       image:"", route:""},
            {title:"",       image:"", route:"500_6"},
            {title:"500.7 Modern Electron Shell Model", image:"", route:"500_7"},          
@@ -71,7 +71,7 @@
            {title:"", image:"", route:""},
            {title:"", image:"", route:""},
            {title:"", image:"", route:""},
-           {title:"600.3 Cracks ", image:"", route:"600_3"}, 
+           {title:"600.3 Atomic Spectra & Rydberg Formula ", image:"", route:"600_3"}, 
            {title:"", image:"", route:""},
            {title:"", image:"", route:""},
            {title:"", image:"", route:""}, 
@@ -83,7 +83,7 @@
            {title:"", image:"", route:""},
            {title:"", image:"", route:""},
            {title:"", image:"", route:""},
-           {title:"700.3 Atomic Spectra & Rydberg Formula", image:"", route:"700_3"}      
+           {title:"", image:"", route:"700_3"}      
         ],
         
     ];
