fa8faf68b3764b226a883494fd5f5f35df9e2f7a
Jonathan <jcsharp@ualberta.ca>
2015-11-18 14:50:08 -0700

    Demo - three Japanese pages with images

--- app/scripts/data/Sandbox.js
+++ app/scripts/data/Sandbox.js
@@ -15,12 +15,27 @@
             {title:"20.3 Interact", image: "", route:"Interact"},           
         ],
   
-    // index 1
+      // index 1
         [
-            {title:"30.1 Japanese", image: "", route:"Japan"},
+            {title:"", image: "", route:""},
           
         ],
-   
+    // index 2
+        [
+            {title:"", image: "", route:""},
+            {title:"Japanese 1-1", image: "", route:"Japan1-1"},       
+        ],
+     // index 3
+        [
+            {title:"", image: "", route:""},
+            {title:"Japanese 1-2", image: "", route:"Japan1-2"},        
+        ], 
+     // index 4
+        [
+            {title:"", image: "", route:""},
+            {title:"Japanese 1-3", image: "", route:"Japan1-3"},        
+        ],        
+        
     ];
 
     scope.Sandbox = s;
