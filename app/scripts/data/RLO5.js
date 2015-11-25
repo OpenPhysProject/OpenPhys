aac92cd84f95e4167462cfb664d5266d513ebf4a
Jonathan <jcsharp@ualberta.ca>
2015-11-25 00:16:22 -0700

    RLO5 - more updates

--- app/scripts/data/RLO5.js
+++ app/scripts/data/RLO5.js
@@ -12,17 +12,23 @@
 
     // index 0,
         [
-            {title:"100 PE Introduction",       image:"", route:"100_0"},
-            {title:"100.1 PE History",  image:"", route:"100_1"},
-            {title:"100.2 Optical PE Effect", image:"", route:"100_2"}, 
-            {title:"", image:"", route:"100_3"},           
-            {title:"", image:"", route:"100_4"}, 
+            {title:"100 PE Introduction",     image:"", route:"100_0"},
+            {title:"100.1 PE History",        image:"", route:"100_1"},
+            {title:"100.2 Scenarios",         image:"", route:"100_2"}, 
+            {title:"100.3 Optical PE Effect", image:"", route:"100_3"},           
+            {title:"100.4 X-ray, Low Z",      image:"", route:"100_4"}, 
             {title:"", image:"", route:"100_5"}, 
             {title:"", image:"", route:"100_6"},
             {title:"", image:"", route:"100_7"},  
             {title:"", image:"", route:"100_8"},           
         ],        
-                 
+     // index 1,
+        [ 
+            {title:"", image:"", route:"200_0"}, 
+            {title:"", image:"", route:"200_1"},
+            {title:"", image:"", route:"200_2"},  
+            {title:"200.3 Quantum Light", image:"", route:"200_3"},           
+        ],                  
         
     ];
 
