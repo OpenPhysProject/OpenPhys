1506f70bed9601d303f9286f5d3aa69082c32358
Jonathan <jcsharp@ualberta.ca>
2015-11-24 20:52:01 -0700

    RLO5: More photoelectric content

--- app/scripts/data/RLO5.js
+++ app/scripts/data/RLO5.js
@@ -1,7 +1,7 @@
 (function (scope) {
     var s = {};
     
-    s.title = "Photoelectric";
+    s.title = "Photoelectric Effect";
     s.preview = "/images/RLO5/PE.svg"; //svg image
     s.route = "PE";
     s.info = "";
@@ -12,15 +12,15 @@
 
     // index 0,
         [
-            {title:"100 Introduction",       image:"", route:"100_0"},
-            {title:"100.1 Terms Explained",  image:"", route:"100_1"},
-            {title:"100.2 E=mc2 as a Conversion between Units", image:"", route:"100_2"}, 
-            {title:"100.3 E=mc2 for a Particle at Rest", image:"", route:"100_3"},           
-            {title:"100.4 E=mc2 for Particles in Motion", image:"", route:"100_4"}, 
-            {title:"100.5 E=mc2 and Binding Energy", image:"", route:"100_5"}, 
-            {title:"100.6 E=mc2 for a Photon?", image:"", route:"100_6"},
-            {title:"100.7 E=mc2 and Antimatter", image:"", route:"100_7"},  
-            {title:"100.8 E=mc2 Summary", image:"", route:"100_8"},           
+            {title:"100 PE Introduction",       image:"", route:"100_0"},
+            {title:"100.1 PE History",  image:"", route:"100_1"},
+            {title:"100.2 Optical PE Effect", image:"", route:"100_2"}, 
+            {title:"", image:"", route:"100_3"},           
+            {title:"", image:"", route:"100_4"}, 
+            {title:"", image:"", route:"100_5"}, 
+            {title:"", image:"", route:"100_6"},
+            {title:"", image:"", route:"100_7"},  
+            {title:"", image:"", route:"100_8"},           
         ],        
                  
         
