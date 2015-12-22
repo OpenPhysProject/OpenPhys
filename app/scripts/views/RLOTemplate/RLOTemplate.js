e646c03f56c8c5c81ad6d2c3b709863c3d748b3e
OJay Robinson <ojayrob@gmail.com>
2015-12-22 15:02:45 -0700

    add quiz handling to default view code

--- app/scripts/views/RLOTemplate/RLOTemplate.js
+++ app/scripts/views/RLOTemplate/RLOTemplate.js
@@ -38,8 +38,40 @@ OER.Views = OER.Views || {};
             } else {
                 this.setElement(this.template());
             }
-           
-        }
+            
+            // check for quiz questions
+            var answers = $(".rlo-content-answer", this.$el);
+            answers.on("click", this.handleAnswer.bind(this));
+            
+            var reveals = $(".rlo-content-reveal", this.$el);
+            reveals.on("click", this.handleReveal.bind(this));
+        },
+        
+        /**
+         * handle an answer being clicked
+         * @method handleAnswer
+         * @param {event} event
+         */
+        handleAnswer: function(event){
+            console.log("answer clicked", event);
+            var answer = event.currentTarget.dataset.answer == "true";
+            if (answer) {
+                event.currentTarget.insertAdjacentHTML("afterend", "  Yes");
+            } else {
+                event.currentTarget.insertAdjacentHTML("afterend", "  No");
+            }
+        },
+        
+        /**
+         * handle a reveal being clicked
+         * @method handleReveal
+         * @param {event} event
+         */
+        handleReveal: function(event) {
+            console.log("answer clicked", event);
+            var answer = event.currentTarget.dataset.answer;
+            event.currentTarget.insertAdjacentHTML("afterend", "  " + answer);
+        },
 
     };
 
