0c6531b0e2db72e4b4917c95a5ab8019c02777d8
Jonathan Sharp <jcsharp@ualberta.ca>
2016-02-24 16:00:44 -0700

    Lesson 2: added radiation to decay animation

--- app/content/lesson2/views/Row3_Col0.js
+++ app/content/lesson2/views/Row3_Col0.js
@@ -17,7 +17,7 @@ OER.Views.Radioactivity = OER.Views.Radioactivity || {};
     p.width = null;         // stage width
     p.height = null;        // stage height
     // Easeljs shapes:
-    p.atoms = null;     // array of easeljs shapes
+   // p.atoms = null;     // array of easeljs shapes
     p.tickerBind = null;    // reference to bound function, binding lets us call back in this scope
     p.buttonBind = null;    // reference to bound function
     
@@ -29,11 +29,24 @@ OER.Views.Radioactivity = OER.Views.Radioactivity || {};
         cloudSize: 11, //12,  
         scale: 1.000, 
         nucleusSize: 2,
+        nucleusColour:        "rgba(100,100,100,1.0)",
+        nucleusColourClicked: "rgba(150,150,150,1.0)",    // colour after clicked        
+        diffusion: 0.0,
+    };    
+  
+    p.radProps1 = {
+        sourceX: 400, // positioning is done by lattice
+        sourceY: 200, 
+        colour:             "rgba(255,255,0,0.5)",
+        cloudColourClicked: "rgba(0,150,0,0.5)",    // colour after clicked
+        cloudSize: 12, //12,  
+        scale: 1.000, 
+        nucleusSize: 3,
         nucleusColour:        "rgba(255,255,255,1.0)",
         nucleusColourClicked: "rgba(150,150,150,1.0)",    // colour after clicked        
         diffusion: 0.0,
     };    
-     
+    
     /**
      * backbone initialize function
      * called on creation by OER.LessonBaseView.updateContent
@@ -62,13 +75,8 @@ OER.Views.Radioactivity = OER.Views.Radioactivity || {};
         if (createjs.Touch.isSupported()) {createjs.Touch.enable(this.stage);}
         this.width  = c.width;
         this.height = c.height;
-        
-        // Attach listener to stage
-        //hmm only works if an object is clicked, not the empty stage
-        //this.stage.on("click",this.stageClick, null, false, null, true);
 
-       //=============== STATIC CONTENT ====================//
-                        
+       //=============== STATIC CONTENT ====================//             
        // Text 
         this.txt = new createjs.Text("DECAYING ATOMS", "18px Arial", "#FFF");
         this.txt.x = 40;  this.txt.y = 10;
@@ -76,8 +84,12 @@ OER.Views.Radioactivity = OER.Views.Radioactivity || {};
         this.stage.addChild(this.txt);
         
         // Draw Particles
-        this.atoms = [];    // create empty array
+        //this.atoms = [];    // create empty array
         this.drawLattice();
+        
+        // test radiation code
+        this.drawRadiation();     
+        
         // set up createjs ticker to update stage
         createjs.Ticker.timingMode = createjs.Ticker.RAF;   // sets ticks to happen on browser request animation frame
         this.tickerBind            = this.tick.bind(this);
@@ -105,6 +117,15 @@ OER.Views.Radioactivity = OER.Views.Radioactivity || {};
         Backbone.View.prototype.remove.call(this, options);
     };
         
+    // ==== RADIATION CODE ===//
+    // produced when a radioactive decay occurs
+    p.drawRadiation = function () {
+        var rad = this.drawRadHelper(this.radProps1);
+        this.stage.addChild(rad);
+        //return rad;
+    };  
+        
+    // ==== ATOMS CODE ===== //    
     p.drawLattice = function() {
         // Draw a 2D array of particles
         var x,y, x_lattice_spacing, y_lattice_spacing, x_lattice_size, y_lattice_size;
@@ -124,10 +145,10 @@ OER.Views.Radioactivity = OER.Views.Radioactivity || {};
             }
         }   
     };    
-           
+                  
     p.addParticle = function(e) {
         this.stage.addChild(e);     // add particle to stage
-        this.atoms.push(e);     // add electron to array
+      //  this.atoms.push(e);     // add electron to array. but is this ever used?
     };
 
     // Draw particle and add it to stage   
@@ -142,8 +163,7 @@ OER.Views.Radioactivity = OER.Views.Radioactivity || {};
         var nucleus  = new createjs.Shape();
         var c_atom   = new createjs.Container();
         var v       = c_atom.tickProps = {};   // Define parameters to be used for each tick     
-
-       // Cloud 
+       // Electron Cloud 
        v.electronFillCommand = electron.graphics.beginFill(Props.colour).command;
        electron.graphics.drawCircle(0, 0, Props.cloudSize); // electron radius
        //
@@ -155,18 +175,50 @@ OER.Views.Radioactivity = OER.Views.Radioactivity || {};
         c_atom.addChild(electron, nucleus);
         c_atom.on("tick",this.atomTick);  // add tick listener to atom, which is called by createjs tick event 
   //      c_atom.on("click",this.atomClick); 
-       
         v.x         = Props.sourceX;  //v.x = 400*Math.random();
         v.y         = Props.sourceY;  //v.y = 200*Math.random();
-        
         v.diffusion = Props.diffusion;      
         v.scale     = Props.scale;
         v.cloudColourClicked   = Props.cloudColourClicked;
         v.nucleusColourClicked = Props.nucleusColourClicked;
+        v.decayed = 0;
         //v.scaleX = 2.0;
         c_atom.x        =v.x;       // update electron position
         c_atom.y        = v.y;      
         return c_atom;
+    };   
+   
+   p.drawRadHelper = function(Props) {
+       // make radiation from 2 parts
+        var electron = new createjs.Shape();
+        var nucleus  = new createjs.Shape();
+        var c_rad   = new createjs.Container();
+        var v       = c_rad.tickProps = {};   // Define parameters to be used for each tick     
+       // Electron Cloud 
+       v.electronFillCommand = electron.graphics.beginFill(Props.colour).command;
+       electron.graphics.drawCircle(0, 0, Props.cloudSize); // electron radius
+       //
+       // Nucleus
+       v.nucleusFillCommand = nucleus.graphics.beginFill(Props.nucleusColour).command;
+       nucleus.graphics.drawCircle(0, 0, Props.nucleusSize); 
+       //
+       //Radiation Particle
+        c_rad.addChild(electron, nucleus);
+        c_rad.on("tick",this.radTick);  // add tick listener to atom, which is called by createjs tick event 
+        v.x         = Props.sourceX;  //v.x = 400*Math.random();
+        v.y         = Props.sourceY;  //v.y = 200*Math.random();
+        
+        var angle = 2*Math.PI * Math.random(); 
+        var speed = 6.0;
+        v.xinc = speed * Math.cos(angle); // 
+        v.yinc = speed * Math.sin(angle); // 
+        v.scale     = Props.scale;
+        v.cloudColourClicked   = Props.cloudColourClicked;
+        v.nucleusColourClicked = Props.nucleusColourClicked;
+        //v.scaleX = 2.0;
+        c_rad.x        =v.x;       // update position
+        c_rad.y        = v.y;      
+        return c_rad;
     };   
    
    // ============== EVOLUTION ================ //
@@ -182,9 +234,18 @@ OER.Views.Radioactivity = OER.Views.Radioactivity || {};
    
     p.atomTick = function () {
         // Decay
-        if (Math.random() > 0.999) {
+        if (Math.random() > 0.999 && this.tickProps.decayed == 0) {
+            // atom changes appearance
+            this.tickProps.decayed = 1;
              this.tickProps.electronFillCommand.style = this.tickProps.cloudColourClicked;
-             this.tickProps.nucleusFillCommand.style  = this.tickProps.nucleusColourClicked;          
+             this.tickProps.nucleusFillCommand.style  = this.tickProps.nucleusColourClicked;  
+            // radiation created 
+            p.radProps1.sourceX = this.x;   // radiation starts of location of decayed atom
+            p.radProps1.sourceY = this.y;
+            var rad = p.drawRadHelper(p.radProps1);
+            this.stage.addChild(rad);
+            
+          // p.drawRadiation();
         }
 //        // Particle moves
 //        if (this.tickProps.x > -10 && this.tickProps.x < 810 &&
@@ -204,6 +265,29 @@ OER.Views.Radioactivity = OER.Views.Radioactivity || {};
 //            // remove this particle from stage if off screen
 //            this.stage.removeChild(this);
 //        };
+    };
+    
+    p.radTick = function () {
+        // radiation moves, and eventually disappears
+    //        
+        if (this.tickProps.x > -10 && this.tickProps.x < 810 &&
+            this.tickProps.y > -10 && this.tickProps.y < 410) {
+            // update Props: "Diffusion"
+            //var speed = 2; // could allow to accelerate?
+            this.tickProps.x += this.tickProps.xinc; //
+            this.tickProps.y += this.tickProps.yinc; //yinc = speed * Math.sin(this.tickProps.angle); // 
+   
+            // update particle:
+           // this.scaleX  *= this.tickProps.scale;   // change size of the particles
+           // this.scaleY  *= this.tickProps.scale;   // 
+            this.x        = this.tickProps.x;       // update electron position
+            this.y        = this.tickProps.y;  
+        }
+        else {
+            // remove this particle from stage if off screen
+            this.stage.removeChild(this);
+        };    
+        
     };
     
     function Hello2() {
