98a8726b2360a1b3fffb5d15958e5eb4299dd67f
Jonathan Sharp <jcsharp@ualberta.ca>
2016-02-25 18:48:05 -0700

    Use automatic numbering for Lesson 1.

--- app/content/lesson1/data.js
+++ app/content/lesson1/data.js
@@ -5,74 +5,76 @@
     s.preview = "/content/lesson1/assets/AtomPlay.svg"; //svg image
     s.route = "ElectronicStructureOfTheAtom";
     s.info = "";
-    s.primaryPathIndex = 2;
+    s.primaryPathIndex = 2;  // 3
+    s.rowLeadNumber = 10;      // index for top row
+    s.rowIncrement = 10;
     
     s.contentMapData = [
         // index 0
         {
             mapCards: [
-                {title:"", image: "", route:"100_0"},
+                {title:"", image:"", route:""},
                 {title:"", image:"", route:""},
                 {title:"", image:"", route:""},
                 {title:"", image:"", route:""},
                 {title:"", image:"", route:""},
             ],
         },   
-        // index 0
+        // index 1
         {
             mapCards: [
-                {title:"", image:"", route:"200_0"},
-                {title:"", image:"", route:"200_1"},
-                {title:"", image:"", route:""},           
-                {title:"Classical Physics (1800s)", number:"200.3", image:"", route:"200_3"},
-                {title:"", image:"", route:""},
-                {title:"Travelling Waves",          number:"200.5", image:"", route:"Waves", icons:["video"]},
-                {title:"", image:"", route:""},
-                {title:"", image:"", route:""},
-                {title:"", image:"", route:""},
+                {route:"",      title:"", image:""},
+                {route:"",      title:"", image:""},
+                {route:"",      title:"", image:""},           
+                {route:"CP",    title:"Classical Physics (1800s)", image:"" },
+                {route:"",      title:"", image:""},
+                {route:"Waves", title:"Travelling Waves", image:"",icons:["video"]},
+                {route:"",      title:"", image:"", },
+                {route:"",      title:"", image:""},
+                {route:"",      title:"", image:""},
             ],
         },  
-        //index 1
+        //index 2
         {
             mapCards: [
-                {title:"", image:"", route:""},
-                {title:"", image:"", route:""},
-                {title:"", image:"", route:""},          
-                {title:"Classical Electro - magnetism",    number:"300.3", image:"", route:"300_3"},
-                {title:"Angular Momentum",                 number:"300.4", image:"", route:"Angular"},
-                {title:"Standing Waves", number:"300.5", image:"", route:"StandingWaves", icons:["video"]},
-                {title:"", image:"", route:""}, 
-                {title:"Quantum Mechanics", number:"300.7", image:"", route:"300_7"},           
-            ],
+                {route:"",              title:""},
+                {route:"",              title:""},
+                {route:"",              title:""},          
+                {route:"EM",            title:"Classical Electro - magnetism"},
+                {route:"Angular",       title:"Angular Momentum",        icons:["video"]},
+                {route:"StandingWaves", title:"Standing Waves",          icons:["video"]},
+                {route:"",              title:""}, 
+                {route:"QM",            title:"Quantum Mechanics"},           
+            ]
         }, 
-        // index 2 
+        // index 3 
         {
             startNode: true,
             endNode: true,
             horizontalLinks: OER.linkType.strong,
             mapCards: [
-               {title:"Atomic Models",                              number:"400.0", image:"", route:"Models", icons:["interaction"]},
-               {title:"Dalton's Atoms (1805)",                      number:"400.1", image:"", route:"Dalton", icons:["interaction"]},
-               {title:"Plum Pudding Atom (1904)",                   number:"400.2", image:"", route:"400_2"},  
-               {title:"Classical Rutherford Nuclear Atom (1910's)", number:"400.3", image:"", route:"SolarSystem",icons:["interaction"]}, 
-               {title:"Quantized Bohr Atom \n(1913)",               number:"400.4", image:"", route:"400_4"}, 
-               {title:"Quantum Atom 1: Matter Waves",               number:"400.5", image:"", route:"QAtomOne", icons:["video"]},
-               {title:"Quantum Atom 2: Nuclear Potential",          number:"400.6", image:"", route:"NuclearPotential"}, 
-               {title:"Quantum Atom 3: Trapped Waves",              number:"400.7", image:"", route:"400_7"},
-               {title:"Beyond the Quantum Atom",                    number:"400.8", image:"", route:"400_8", icons:["quiz"]},          
+               {route:"Models",      title:"Atomic Models",     icons:["interaction"]},
+               {route:"Dalton",      title:"Dalton's Atoms (1805)",                      icons:["interaction"]},
+               {route:"Plum",        title:"Plum Pudding Atom (1904)"},  
+               {route:"SolarSystem", title:"Classical Rutherford Nuclear Atom (1910's)",  icons:["interaction"]}, 
+               {route:"Bohr",        title:"Quantized Bohr Atom \n(1913)" }, 
+               {route:"QAtomOne",    title:"Quantum Atom 1: Matter Waves",               icons:["video"]},
+               {route:"Potential",   title:"Quantum Atom 2: Nuclear Potential" }, 
+               {route:"Trap",        title:"Quantum Atom 3: Trapped Waves" },
+               {route:"Beyond",      title:"Beyond the Quantum Atom",                    icons:["quiz"]},          
             ],
         },
-        // index 3 
+        // index 4 
         {
             mapCards: [
-               {title:"",       image:"", route:"500_0"},
-               {title:"",       image:"", route:"500_1"},
-               {title:"",       image:"", route:"500_2"},
-               {title:"Classical Atom - Failures", number:"500.3", image:"", route:"500_3"},
-               {title:"", image:"", route:"500_4"}, 
-               {title:"", image:"", route:""},
-               {title:"", image:"", route:"500_6"},
-               {title:"Modern Electron Shell Model", number:"500.7", image:"", route:"500_7"},          
+               {title:"",                           image:"", route:"500_0"},
+               {title:"",                           image:"", route:"500_1"},
+               {title:"",                           image:"", route:"500_2"},
+               {title:"Classical Atom - Failures",  image:"", route:"Fail"},
+               {title:"",                           image:"", route:"500_4"}, 
+               {title:"",                           image:"", route:""},
+               {title:"",                           image:"", route:"500_6"},
+               {title:"Modern Electron Shell Model", image:"", route:"Shell"},          
             ], 
         },
         // index 4  
@@ -81,11 +83,11 @@
                {title:"", image:"", route:""},
                {title:"", image:"", route:""},
                {title:"", image:"", route:""},
-               {title:"Atomic Spectra & Rydberg Formula", number:"600.3", image:"", route:"600_3"}, 
+               {title:"Atomic Spectra & Rydberg Formula", image:"", route:"Spectra"}, 
                {title:"", image:"", route:""},
                {title:"", image:"", route:""},
                {title:"", image:"", route:""}, 
-               {title:"Successes of Quantum Atom", number:"600.7", image:"", route:"600_7"},          
+               {title:"Successes of Quantum Atom", image:"", route:"Success"},          
             ],
         },
           // index 6 
