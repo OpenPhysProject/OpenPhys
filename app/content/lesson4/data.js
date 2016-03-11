8115d71f5901487b1413ae2894f68e3e67870b62
Jonathan Sharp <jcsharp@ualberta.ca>
2016-03-10 21:37:12 -0700

    Lesson 4: minor

--- app/content/lesson4/data.js
+++ app/content/lesson4/data.js
@@ -1,10 +1,10 @@
 (function (scope) {
     var s = {};
     
-    s.title = "Photoelectric Effect";
-    s.preview = "/content/lesson5/assets/icon_photoelectriceffect.svg"; //svg image
-    s.themeColor = "#FFC107";
-    s.route = "PE";
+    s.title = "Electromagnetic Spectrum";
+    //s.preview = "/content/lesson4/assets/icon_photoelectriceffect.svg"; //svg image
+    s.themeColor = "#E91E63"; // straight outta' Compton
+    s.route = "EM";
     s.info = "";
     s.jumpNav = false;    
     s.primaryPathIndex = 0;
@@ -16,11 +16,11 @@
             endNode: true, 
             horizontalLinks: OER.linkType.strong,
             mapCards: [
-                {route:"100_0", title:"PE Introduction"  },
-                {route:"100_1", title:"PE History"       },
-                {route:"100_2", title:"Scenarios"        }, 
-                {route:"100_3", title:"Optical PE Effect"},           
-                {route:"100_4", title:"X-ray, Low Z"     },            
+                {route:"100_0", title:"EM Introduction"  },
+                {route:"100_1", title:"EM History"       },
+//                {route:"100_2", title:"Scenarios"        }, 
+//                {route:"100_3", title:"Optical PE Effect"},           
+//                {route:"100_4", title:"X-ray, Low Z"     },            
             ],    
         },
          // index 1,
@@ -29,11 +29,11 @@
                 {route:"200_0", title:"" }, 
                 {route:"200_1", title:"" },
                 {route:"200_2", title:"" },  
-                {route:"200_3", title:"Quantum Light"},           
+              //  {route:"200_3", title:"Quantum Light"},           
             ],                  
         },
     ];
 
-    scope.lesson5 = s;
+    scope.lesson4 = s;
 
 }(window.OER.data = window.OER.data || {}));
