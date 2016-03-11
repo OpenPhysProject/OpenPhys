5af2d7f180fb2a16ff75bf05a76b6bc3c7ad23f9
Jonathan Sharp <jcsharp@ualberta.ca>
2016-03-10 21:36:38 -0700

    Started on Lesson 4

---
+++ app/content/lesson4/data.js
@@ -0,0 +1,39 @@
+(function (scope) {
+    var s = {};
+    
+    s.title = "Photoelectric Effect";
+    s.preview = "/content/lesson5/assets/icon_photoelectriceffect.svg"; //svg image
+    s.themeColor = "#FFC107";
+    s.route = "PE";
+    s.info = "";
+    s.jumpNav = false;    
+    s.primaryPathIndex = 0;
+    
+    s.contentMapData = [  
+        // index 0,
+        {
+            startNode: true,
+            endNode: true, 
+            horizontalLinks: OER.linkType.strong,
+            mapCards: [
+                {route:"100_0", title:"PE Introduction"  },
+                {route:"100_1", title:"PE History"       },
+                {route:"100_2", title:"Scenarios"        }, 
+                {route:"100_3", title:"Optical PE Effect"},           
+                {route:"100_4", title:"X-ray, Low Z"     },            
+            ],    
+        },
+         // index 1,
+         {
+            mapCards: [ 
+                {route:"200_0", title:"" }, 
+                {route:"200_1", title:"" },
+                {route:"200_2", title:"" },  
+                {route:"200_3", title:"Quantum Light"},           
+            ],                  
+        },
+    ];
+
+    scope.lesson5 = s;
+
+}(window.OER.data = window.OER.data || {}));
