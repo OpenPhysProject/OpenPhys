f43fb17abfcc351b65314c1bf795b7961b9277cf
Jonathan Sharp <jcsharp@ualberta.ca>
2016-03-14 22:14:08 -0600

    Lesson 1: angular momentum aim updated

--- app/content/lesson1/views/Angular.js
+++ app/content/lesson1/views/Angular.js
@@ -27,11 +27,11 @@ OER.Views.ElectronicStructureOfTheAtom = OER.Views.ElectronicStructureOfTheAtom 
  
     p.electronProps = {
         angle:    0.0,           // initial angle, with
-        angleInc: 0.02,  // 0.04
+        angleInc: 0.01,  // 0.04
         originX: 400,    originY: 200,
         radius:  100,  //  Orbital Radius
-        colour: "rgba(200,255,200,1.0",
-        size: 90
+        colour: "rgba(200,255,100,1.0",
+        size: 50
     }; 
     
     /**
@@ -55,7 +55,7 @@ OER.Views.ElectronicStructureOfTheAtom = OER.Views.ElectronicStructureOfTheAtom 
         } else {
             this.setElement(this.template());
         }
-
+        var e = this.electronProps;
         // setup createjs stage and touch support
         var c = $(".lesson-content-canvas", this.$el)[0];
         this.stage = new createjs.Stage(c);
@@ -64,19 +64,21 @@ OER.Views.ElectronicStructureOfTheAtom = OER.Views.ElectronicStructureOfTheAtom 
         this.height = c.height;
         
        // draw Static circle (nucleus)
-        this.nucleus = new createjs.Shape();
-        this.nucleus.graphics.beginFill("rgba(200,255,200,1.0").drawCircle(0, 0, 20);
-        this.nucleus.x =  this.electronProps.originX; // x position
-        this.nucleus.y =  this.electronProps.originY;
-        this.stage.addChild(this.nucleus);  // add this shape to the stage
+//        this.nucleus = new createjs.Shape();
+//        this.nucleus.graphics.beginFill(e.colour).drawRect(-15,-15,30,30);//;drawCircle(0, 0, 20);
+//        this.nucleus.x =  e.originX; // x position
+//        this.nucleus.y =  e.originY;
+//        this.stage.addChild(this.nucleus);  // add this shape to the stage
         
         // draw circle (electron)
+        this.electronProps.angle = 0.0;
+        this.makeBar();
         this.electrons = [];    // create empty array
         this.electronProps.angle = 0.0;
-        this.handleButton();
+        this.makeElectron();
         this.electronProps.angle = Math.PI;
-        this.handleButton();
-
+        this.makeElectron();
+ 
         // set up createjs ticker to update stage
         createjs.Ticker.timingMode = createjs.Ticker.RAF;   // sets ticks to happen on browser request animation frame
         this.tickerBind = this.tick.bind(this);
@@ -90,6 +92,8 @@ OER.Views.ElectronicStructureOfTheAtom = OER.Views.ElectronicStructureOfTheAtom 
      */
     p.tick = function(event) {
         // update the stage
+       // p.nucleus.rotation += 1;
+        
         this.stage.update(event);   // redraw shapes on the stage
     };
 
@@ -105,13 +109,16 @@ OER.Views.ElectronicStructureOfTheAtom = OER.Views.ElectronicStructureOfTheAtom 
         Backbone.View.prototype.remove.call(this, options);
     };
     
-    /**
-     * click listener for button
-     */
-    p.handleButton = function() {
+    p.makeElectron = function() {
         var e = this.drawElectron();
         this.stage.addChild(e); // add electron to stage
         this.electrons.push(e); // add electron to array
+    };
+
+    p.makeBar = function() {
+        var e = this.drawBar();
+        this.stage.addChild(e); // add electron to stage
+        //this.electrons.push(e); // add electron to array
     };
 
 // ELECTRON CODE *********************************************************************************
@@ -137,7 +144,27 @@ OER.Views.ElectronicStructureOfTheAtom = OER.Views.ElectronicStructureOfTheAtom 
         return electron;
     };
    
-   // ============== EVOLUTION ================ //
+    p.drawBar = function() {
+        // Actual Orbital radius
+        var e = this.electronProps; // convenience
+       // var radius = Math.round(e.radius); // orbital radius
+        
+        var bar = new createjs.Shape();
+        bar.graphics.beginFill("darkred").drawRect(-20/2,-150/2,20,150); //.drawCircle(400, 200, 30); // electron radius
+        bar.x =  e.originX;           // initial x position
+        bar.y =  e.originY;// + radius;
+        bar.on("tick",this.barTick);  // add tick listener to electron, which is called by createjs tick event
+        
+        // Define parameters to be used for each tick
+        var v = bar.tickProps = {};
+        v.originX     = e.originX;
+        v.originY     = e.originY;
+        v.angle       = e.angle; // Math.random()*e.angleDelta    + e.angle;
+        v.angleInc    = e.angleInc; //Math.random()*e.angleIncDelta + e.angleInc;
+      //  v.radius      = radius;
+        return bar;
+    };  
+   
     p.electronTick = function () {
         // Electron spirals in to nucleus
         var t = this.tickProps;
@@ -152,6 +179,16 @@ OER.Views.ElectronicStructureOfTheAtom = OER.Views.ElectronicStructureOfTheAtom 
         this.x = t.originX + t.radius*Math.sin(t.angle);  // 
         this.y = t.originY + t.radius*Math.cos(t.angle);
         t.angle += t.angleInc;
+    };
+
+    p.barTick = function () {
+        // Electron spirals in to nucleus
+      var t = this.tickProps;
+        
+     this.rotation = 180*t.angle/Math.PI;//-= 1;
+       // this.x = t.originX + t.radius*Math.sin(t.angle);  // 
+       // this.y = t.originY + t.radius*Math.cos(t.angle);
+        t.angle -= t.angleInc;
     };
 
     // add the above code as a backbone view class in our namespace
