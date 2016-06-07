d53e5883fb00562b75cd99a40ff5cd172a040e44
Stephanie Yong <syong@ualberta.ca>
2016-06-07 11:47:57 -0600

    Lesson 2a: big changes to mapping rearrangement. Some lesson tiles combined, but overall columns remain the same.

--- app/content/lesson2a/data.js
+++ app/content/lesson2a/data.js
@@ -28,33 +28,33 @@
         // index 1
         {
             mapCards: [
-                {route: "AtomEverything",   template:"AtomsEvery.ejs",   title: "(almost) everything is made from atoms", group:"atoms"},
-                {route: "AtomicNucleus",    template:"AtomicNucleus.ejs",   title: "the atomic nucleus",                  group:"nuclei"},
+                {route: "AtomTypes",     template:"AtomsTypes.ejs",    title: "different types of atom", group:"atoms" },
+                {route: "PeriodicTable", template:"PeriodicTable.ejs", title: "periodic table",          group:"nuclei"},                
                 {route: "LabelingNuclides", template:"LabelingNuclides.ejs", title: "labeling nuclides",       group:"nuclides"}, 
-                {route: "NucleiRadiate",    template:"NucleiRadiate.ejs",    title: "nuclei can radiate",      group:"radiation"},
-                {route: "SleeperAtHeart",   template:"SleeperAtHeart.ejs", title: "the sleeper at the heart",  group:"structure"},               
+                {title:""},
+                {route: "Shell",         template:"Shell.ejs",         title: "shell model of nuclear structure", group:"structure"},               
               //  {route: "RadionuclidesIRL", template:"RadionuclidesIRL.ejs",    title: "radionuclides in real life", group:"world" }
             ],
         },
         //index 2
         { 
             mapCards: [
-                {route: "AtomTypes",     template:"AtomsTypes.ejs",    title: "different types of atom", group:"atoms" },
-                {route: "PeriodicTable", template:"PeriodicTable.ejs", title: "periodic table",          group:"nuclei"},
+                {route: "AtomPower",  template:"AtomsPower.ejs",       title: "the power of atoms", group:"atoms" },
+                {route: "InsideNucleus", template:"InsideNucleus.ejs", title: "inside the nucleus",  group:"nuclei"},
                 {title:""},
                 {title:""},
-                {route: "Shell",         template:"Shell.ejs",         title: "shell model of nuclear structure", group:"structure"},
+               {route: "LiquidDropModel",template:"LiquidDropModel.ejs",  title: "liquid drop model", group:"structure"},
             //    {route: "RadiationInWorld",  template:"RadiationInWorld.ejs", title: "radiation in the world", group:"world " }
             ],
         },
         //index 3
-        {
-            mapCards: [
-                {route: "AtomPower",  template:"AtomsPower.ejs",       title: "the power of atoms", group:"atoms" },            
-                {route: "InsideNucleus", template:"InsideNucleus.ejs", title: "inside the nucleus",  group:"nuclei"},
-                {title:""},
-                {title:""},
-                {route: "LiquidDropModel",template:"LiquidDropModel.ejs",  title: "liquid drop model", group:"structure"}
+//        {
+//            mapCards: [
+//                {route: "AtomPower",  template:"AtomsPower.ejs",       title: "the power of atoms", group:"atoms" },            
+//                {route: "InsideNucleus", template:"InsideNucleus.ejs", title: "inside the nucleus",  group:"nuclei"},
+//                {title:""},
+//                {title:""},
+//                {route: "LiquidDropModel",template:"LiquidDropModel.ejs",  title: "liquid drop model", group:"structure"}
              //   {route: "StructureIRL",   template:"StructureIRL.ejs",     title: "nuclear structure in the real world",group:"world"},               
             ]
         },
