fea51f736ca0bfec524031c314d7b0bd5af5a1bd
Stephanie Yong <syong@ualberta.ca>
2016-08-24 10:29:19 -0600

    Lesson 3 - icon title fix

--- app/content/lesson3/data.js
+++ app/content/lesson3/data.js
@@ -1,7 +1,7 @@
 (function (scope) {
     var s = {};
     
-    s.title = "E = mc2";
+    s.title = "E = mc<sup>2</sup>";
     s.preview = "/content/lesson3/assets/icon_Emc2.svg"; //svg image
     s.themeColor = "#4CAF50";
     s.route = "Emc2";
